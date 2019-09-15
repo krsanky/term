974993d75a090215f6d89cf18fbc8496a4957422
paul wisehart <paul@oldcode.org>
2019-09-15 12:23:56 -0400

    termion example runs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use termion::{color, style};
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}Red", color::Fg(color::Red));
+    println!("{}Blue", color::Fg(color::Blue));
+    println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
+    println!("{}Just plain italic", style::Italic);
 }
